cd5c3b185d2b08a446eaf0016238bed9a7f3948c
Ridhwanul Haque <rhemon19@gmail.com>
2016-11-09 12:26:45 +0600

    Welcome

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ var stickyNavTop = $('.navbar').offset().top;
 $(window).scroll(function() {
     if ($(window).scrollTop() > stickyNavTop) {
         $('.navbar').addClass('navbar-fixed-top');
+        $('#img-slider').height('80vh');
     }
     if ($(window).scrollTop() < stickyNavTop) {
         $('.navbar').removeClass('navbar-fixed-top');
+        $('#img-slider').height('75vh');
     }
 });
